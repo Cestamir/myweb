cfb6418fdc0649ea9399651d476c86faa1dbdb7a
Cestamir <cestmirpavlasek@gmail.com>
2025-11-21 19:44:25 +0100

    upadted title section and changed font

--- components/TitleSectionPage.tsx
+++ components/TitleSectionPage.tsx
@@ -4,14 +4,32 @@ const TitleSectionPage = () => {
   return (
     <div className='page title-page'>
         <div className='title-grid'>
-            <div className='auto-blur headline'>CESTMIR</div>
-            <div className='auto-blur headline'>DEVELOPER</div>
-            <div className='auto-blur headline'>THINKER</div>
-            <div className='auto-blur headline'>SUBSCRIBE</div>
-            <div className='auto-blur headline'>SEE MORE</div>
+            <div className='item-a text-box auto-blur headline'>
+                <span className="text-line text-top">DEVELOPER</span>
+                <span className="text-line text-bottom">DEVELOPER</span>
+            </div>
+            <div className='item-b text-box auto-blur headline'>
+                <span className="text-line text-top">THINKER</span>
+                <span className="text-line text-bottom">THINKER</span>
+            </div>
+            <div className='item-c text-box auto-blur headline'>
+                <span className="text-line text-top">DEVELOPER</span>
+                <span className="text-line text-bottom ">DEVELOPER</span>
+            </div>
+            <div className='item-d text-box auto-blur headline'>
+                <span className="text-line text-top">SEE MORE</span>
+                <span className="text-line text-bottom">SEE MORE</span>
+            </div>
+            <div className='item-e text-box auto-blur headline'>
+                <span className="text-line text-top">SUBSCRIBE</span>
+                <span className="text-line text-bottom">SUBSCRIBE</span>
+            </div>
         </div>
     </div>
   )
 }
 
 export default TitleSectionPage
+
+
+
